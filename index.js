0fc75cd6c8a76b8dc556b80d0d8d6512cc9d345a
Nathan Buchar <nathan.buchar@hellosign.com>
2018-08-17 12:25:32 -0700

    [class-methods-use-this] Do not force that class methods use "this"
    Abstracting out class methods which don't access "this" into static methods is more DRY, however it can make class inheritance difficult sometimes.

--- index.js
+++ index.js
@@ -15,5 +15,8 @@ module.exports = {
     // to use "foo" because it makes such and such easier
     // without adding unnecessary complexity.
     // 'no-foo': 'off',
+
+    // This makes class inheritance annoying sometimes.
+    'class-methods-use-this': 'off',
   },
 };
